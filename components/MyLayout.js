bbf5cfe64f060507d456a4aac10d0a6f0e11ff22
bengyles <bengyles@hotmail.com>
2019-11-07 16:08:16 +0100

    deleted unnecessary code

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,11 +1,5 @@
 import Header from './Header';
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD',
-};
-
 const Layout = props => (
     <html lang="en">
     <head>
