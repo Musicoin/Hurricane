a6ba51c435f647828a332e83e4dd21383db1a39f
bengyles <bengyles@hotmail.com>
2019-12-19 17:40:21 +0100

    Got rid of warning by renaming class to className

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,12 @@ import {Query} from 'react-apollo';
 function Home() {
   return (
       <Layout>
-        <div class="content__container-left">
+        <div className="content__container-left">
           <MenuCard/>
           <SocialCard/>
           <MobileAppCard/>
         </div>
-        <div class="content__container-middle">
+        <div className="content__container-middle">
           <Query query={RecentPlaysQuery} variables={{limit: 10}}>
             {({loading, error, data, subscribeToMore}) => {
               if (loading) return <p>Loading...</p>;
@@ -44,69 +44,69 @@ function Home() {
 
             }}
           </Query>
-          {/*  <div class="card">*/}
-          {/*    <div class="card__title">*/}
+          {/*  <div className="card">*/}
+          {/*    <div className="card__title">*/}
           {/*      <p>Top Tips</p>*/}
-          {/*      <div class="card__filter">*/}
-          {/*        <a href="#" class="card__filter-link active">Day |</a>*/}
-          {/*        <a href="#" class="card__filter-link">Week |</a>*/}
-          {/*        <a href="#" class="card__filter-link">Month </a>*/}
+          {/*      <div className="card__filter">*/}
+          {/*        <a href="#" className="card__filter-link active">Day |</a>*/}
+          {/*        <a href="#" className="card__filter-link">Week |</a>*/}
+          {/*        <a href="#" className="card__filter-link">Month </a>*/}
           {/*      </div>*/}
           {/*    </div>*/}
-          {/*    <div class="card__content">*/}
-          {/*      <div class="top-tips">*/}
-          {/*        <div class="track-container">*/}
+          {/*    <div className="card__content">*/}
+          {/*      <div className="top-tips">*/}
+          {/*        <div className="track-container">*/}
           {/*          <Track image="/img/tracks/tracks-1.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1200} tips={3400}/>*/}
           {/*          <Track image="/img/tracks/tracks-2.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1234} tips={2345}/>*/}
           {/*          <Track image="/img/tracks/tracks-3.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={3456} tips={4567}/>*/}
-          {/*          <div class="card__more">*/}
-          {/*            <a href="#" class="card__more-link">See more</a>*/}
+          {/*          <div className="card__more">*/}
+          {/*            <a href="#" className="card__more-link">See more</a>*/}
           {/*          </div>*/}
           {/*        </div>*/}
           {/*      </div>*/}
           {/*    </div>*/}
           {/*  </div>*/}
-          {/*  <div class="card">*/}
-          {/*    <div class="card__title">*/}
+          {/*  <div className="card">*/}
+          {/*    <div className="card__title">*/}
           {/*      <p>Top Played</p>*/}
-          {/*      <div class="card__filter">*/}
-          {/*        <a href="#" class="card__filter-link">Day |</a>*/}
-          {/*        <a href="#" class="card__filter-link active">Week |</a>*/}
-          {/*        <a href="#" class="card__filter-link">Month </a>*/}
+          {/*      <div className="card__filter">*/}
+          {/*        <a href="#" className="card__filter-link">Day |</a>*/}
+          {/*        <a href="#" className="card__filter-link active">Week |</a>*/}
+          {/*        <a href="#" className="card__filter-link">Month </a>*/}
           {/*      </div>*/}
           {/*    </div>*/}
-          {/*    <div class="card__content">*/}
-          {/*      <div class="top-tips">*/}
-          {/*        <div class="track-container">*/}
+          {/*    <div className="card__content">*/}
+          {/*      <div className="top-tips">*/}
+          {/*        <div className="track-container">*/}
           {/*          <Track image="/img/tracks/tracks-1.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1200} tips={3400}/>*/}
           {/*          <Track image="/img/tracks/tracks-2.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1234} tips={2345}/>*/}
           {/*          <Track image="/img/tracks/tracks-3.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={3456} tips={4567}/>*/}
-          {/*          <div class="card__more">*/}
-          {/*            <a href="#" class="card__more-link">See more top tips track</a>*/}
+          {/*          <div className="card__more">*/}
+          {/*            <a href="#" className="card__more-link">See more top tips track</a>*/}
           {/*          </div>*/}
           {/*        </div>*/}
           {/*      </div>*/}
           {/*    </div>*/}
           {/*  </div>*/}
-          {/*  <div class="card">*/}
-          {/*    <div class="card__title">*/}
+          {/*  <div className="card">*/}
+          {/*    <div className="card__title">*/}
           {/*      <p>New Releases</p>*/}
           {/*    </div>*/}
-          {/*    <div class="card__content">*/}
-          {/*      <div class="top-tips">*/}
-          {/*        <div class="track-container">*/}
+          {/*    <div className="card__content">*/}
+          {/*      <div className="top-tips">*/}
+          {/*        <div className="track-container">*/}
           {/*          <Track image="/img/tracks/tracks-1.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1200} tips={3400}/>*/}
           {/*          <Track image="/img/tracks/tracks-2.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={1234} tips={2345}/>*/}
           {/*          <Track image="/img/tracks/tracks-3.png" title="Track's title" artist="Artist's name" genre="Electronic, Alternative" likes={3456} tips={4567}/>*/}
-          {/*          <div class="card__more">*/}
-          {/*            <a href="#" class="card__more-link">See more top new releases</a>*/}
+          {/*          <div className="card__more">*/}
+          {/*            <a href="#" className="card__more-link">See more top new releases</a>*/}
           {/*          </div>*/}
           {/*        </div>*/}
           {/*      </div>*/}
           {/*    </div>*/}
           {/*  </div>*/}
         </div>
-        <div class="content__container-right">
+        <div className="content__container-right">
           <AOWCard/>
           <NewArtistCard/>
           <EventsCard/>
