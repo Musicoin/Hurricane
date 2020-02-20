e8d7c190f7bc12953fb9676328a71c0d8f332ea1
bengyles <bengyles@hotmail.com>
2020-02-20 14:47:32 +0100

    Added links

--- components/Cards/MenuCard.js
+++ components/Cards/MenuCard.js
@@ -7,19 +7,19 @@ export default function MenuCard(){
         <div className="card__content">
           <div className="menu">
             <ul className="menu__list">
-              <a href="#">
+              <a href="https://musicoin.org/how-it-works">
                 <li className="menu__link">What is Musicoin?</li>
               </a>
-              <a href="#">
+              <a href="https://musicoin.org/team">
                 <li className="menu__link">Team</li>
               </a>
-              <a href="#">
+              <a href="https://drive.google.com/file/d/1KVvcwPKUngMNffgWW65k1p4UvKg5QG0u/view">
                 <li className="menu__link">Whitepaper</li>
               </a>
-              <a href="#">
+              <a href="https://musicoin.org/legal/legal">
                 <li className="menu__link">Legal</li>
               </a>
-              <a href="#">
+              <a href="https://musicoin.org/resources/faq">
                 <li className="menu__link">FAQ</li>
               </a>
             </ul>
--- components/Cards/SocialCard.js
+++ components/Cards/SocialCard.js
@@ -7,22 +7,22 @@ export default function SocialCard(){
         <div className="card__content">
           <div className="social">
             <ul className="social__list">
-              <a href="#">
+              <a href="https://twitter.com/musicoins">
                 <li className="social__link"><img src="/img/icons/twitter.png" className="social__icon" alt=""/> Twitter</li>
               </a>
-              <a href="#">
+              <a href="https://reddit.com/r/musicoin">
                 <li className="social__link"><img src="/img/icons/reddit.png" className="social__icon" alt=""/> Reddit</li>
               </a>
-              <a href="#">
+              <a href="https://medium.com/@musicoin">
                 <li className="social__link"><img src="/img/icons/medium.png" className="social__icon" alt=""/>Medium</li>
               </a>
-              <a href="#">
+              <a href="https://bitcointalk.org/index.php?topic=1776113.0">
                 <li className="social__link"><img src="/img/icons/bitcoin-talk.png" className="social__icon" alt=""/>Bitcoin Talk</li>
               </a>
-              <a href="#">
+              <a href="https://github.com/musicoin">
                 <li className="social__link"><img src="/img/icons/github.png" className="social__icon" alt=""/>Github</li>
               </a>
-              <a href="#">
+              <a href="https://www.facebook.com/lovemusicoin/">
                 <li className="social__link"><img src="/img/icons/facebook.png" className="social__icon" alt=""/> Facebook</li>
               </a>
             </ul>
