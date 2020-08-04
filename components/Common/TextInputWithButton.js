a480bcf2ba1be8bdeca87d1c022758a4c3207f5f
bengyles <bengyles@hotmail.com>
2020-08-04 17:05:07 +0200

    Changed colors of the inputbox and button

--- components/Common/TextInputWithButton.js
+++ components/Common/TextInputWithButton.js
@@ -5,7 +5,7 @@ const Text = styled.input`
   height: 20px;
   border-style: solid;
   border-width: 2px;
-  border-color: rgba(0,0,0,0.15);
+  border-color: #E6ECF0;
   padding: 10px;
   padding-right: 50px;
 `;
@@ -14,7 +14,7 @@ const Button = styled.input`
   margin-left: -50px;
   height: 25px;
   width: 40px;
-  background: rgba(0,0,0,0.15);
+  background: #E6ECF0;
   -webkit-appearance: none;
   border: 0;
 `;
